de9af0b2b03e2462a2896ccb4d38d3cb3ec2daf9
Caleb Stromberg <calebstromberg@gmail.com>
2023-02-01 16:42:03 -0800

    Fix inability to click character caption

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -7,8 +7,9 @@ export function CharacterCard(props) {
       <button className="character-card-button" onClick={props.onClick}>
         <img src={props.img} alt={props.rawName}></img>
       </button>
-      <span className="character-card-caption-border">
+      <span className="character-card-caption-border" onClick={props.onClick}>
         <button
+          onClick={props.onClick}
           className="character-card-caption"
           dangerouslySetInnerHTML={{ __html: props.rawName }}
         ></button>
